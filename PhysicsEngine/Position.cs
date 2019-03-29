5b46730dd46d1c70067169630862a323783044f3
Felix Söderman <felixsoderman@gmail.com>
2019-03-29 19:14:40 +0100

    Added Vector2 Maths
    Added vector addition and vector multiplication

--- PhysicsEngine/Position.cs
+++ PhysicsEngine/Position.cs
@@ -34,6 +34,16 @@ namespace PhysicsEngine
                 this.X = X;
                 this.Y = Y;
             }
+
+            public static Vector2 operator* (Vector2 v1, Vector2 v2)
+            {
+                return new Vector2(v1.X * v2.X, v1.Y * v2.Y);
+            }
+
+            public static Vector2 operator +(Vector2 v1, Vector2 v2)
+            {
+                return new Vector2(v1.X + v2.X, v1.Y + v2.Y);
+            }
         }
     }
 }
